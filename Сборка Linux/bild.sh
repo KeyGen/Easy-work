a4f34a02db670d90ebfa5efe096a6a9c7325ca49
KeyGen <keygenqt@gmail.com>
2012-11-17 01:07:08 +0200

    Скрипт для сборки deb

---
+++ Сборка Linux/bild.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+sudo chmod 775 easy_work/usr/share/EasyWork/Style/anime.style
+sudo chmod 775 easy_work/usr/share/EasyWork/Style/brown.style
+sudo chmod 775 easy_work/usr/share/EasyWork/Style/green.style
+sudo chmod 775 easy_work/usr/share/EasyWork/Style/magenta.style
+sudo chmod 775 easy_work/usr/share/EasyWork/KeyboardLayout/be-BY.language
+sudo chmod 775 easy_work/usr/share/EasyWork/KeyboardLayout/de-DE.language
+sudo chmod 775 easy_work/usr/share/EasyWork/KeyboardLayout/en-US.language
+sudo chmod 775 easy_work/usr/share/EasyWork/KeyboardLayout/it-IT.language
+sudo chmod 775 easy_work/usr/share/EasyWork/KeyboardLayout/ka-GE.language
+sudo chmod 775 easy_work/usr/share/EasyWork/KeyboardLayout/ru-RU.language
+sudo chmod 775 easy_work/usr/share/EasyWork/KeyboardLayout/uk-UA.language
+sudo chmod 775 easy_work/usr/share/applications/EasyWork.desktop
+sudo chmod 775 easy_work/usr/share/pixmaps/easyworkgeneral.png
+sudo chmod 775 easy_work/usr/share/EasyWork/Database
+sudo chmod 775 easy_work/usr/share/EasyWork/KeyboardLayout
+sudo chmod 775 easy_work/usr/share/EasyWork/Style
+
+if [ -f easy_work/usr/share/EasyWork/Database/statisticForRegimeFile.mdb ]; then
+  sudo rm easy_work/usr/share/EasyWork/Database/statisticForRegimeFile.mdb
+fi
+
+kate easy_work/DEBIAN/control
+kate easy_work/DEBIAN/changelog
+
+echo "Новая версия:"
+read newVersion
+
+fakeroot dpkg-deb --build easy_work
+
+mv easy_work.deb EasyWork_"$newVersion"_all.deb
